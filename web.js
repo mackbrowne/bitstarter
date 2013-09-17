894a001038c89d52e2f6646e54d25e2e678750ee
MackBrowne <mackbrowne@gmail.com>
2013-09-17 02:21:33 +0000

    added index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var data = fs.readFileSync('message.txt', 'utf8');
+  var data = fs.readFileSync('index.html', 'utf8');
   response.send(data);
 });
 
