2fba55fbe2fda9465ece5dd76a8263979d4caa99
MackBrowne <mackbrowne@gmail.com>
2013-09-17 01:27:34 +0000

    added file reader instead of hardcoded message

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! It is I, Mack Browne.  King nerd of the Internets!');
+  fs.readFileSync('message.txt', function(err, data){
+    if (err) throw err;
+    var buf = new Buffer(data);
+    response.send(buf.toString());
+  }); 
+
 });
 
 var port = process.env.PORT || 5000;
